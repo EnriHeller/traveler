55cb38db64e962d22b69b15ea8ebaef315e61ead
Max <mdelarosatoroag@gmail.com>
2021-08-19 17:44:22 -0500

    se creo un endpoint PUT para actualizar informacion de paquetes turisticos por parametro de id

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ const validarCamposPaquete = (req, res, next) => {
         descripcion,
         duracionDiasPaquete,
         precio,
-        url,
+        imgUrl,
         stock,
     } = req.body;
 
@@ -79,7 +79,7 @@ const validarCamposPaquete = (req, res, next) => {
         descripcion == null || descripcion == "" ||
         duracionDiasPaquete == null || duracionDiasPaquete == "" ||
         precio == null || precio == "" ||
-        url == null || url == "" ||
+        imgUrl == null || imgUrl == "" ||
         stock == null || stock == ""
     ) {
         res.status(400).json({error: `Debe ingresar todos los campos.`})
@@ -300,7 +300,7 @@ async (req, res) => {
             descripcion,
             duracionDiasPaquete,
             precio,
-            url,
+            imgUrl,
             stock,
         } = req.body;
 
@@ -310,7 +310,7 @@ async (req, res) => {
             descripcion,
             duracionDiasPaquete,
             precio,
-            url,
+            imgUrl,
             stock,
             activo: true,
         });
@@ -323,6 +323,39 @@ async (req, res) => {
         console.error(error.message);
         res.status(400).json(error);
     }
+});
+
+//actualizar paquete turistico por id
+server.put("/paquetes-turisticos/:idPaquete",
+validarAdministrador,
+validarCamposPaquete,
+async (req, res) => {
+    const filter = req.params.idPaquete;
+    
+    const {
+        nombreDestino,
+        descripcion,
+        duracionDiasPaquete,
+        precio,
+        imgUrl,
+        stock,
+    } = req.body;
+
+    const update = {
+        nombreDestino,
+        descripcion,
+        duracionDiasPaquete,
+        precio,
+        imgUrl,
+        stock,
+    }
+
+
+    const paqueteDB = await PaquetesTuristicos.findOneAndUpdate(filter, update, {
+        new: true
+    });
+
+    res.status(200).json(paqueteDB);
 });
 
 //Conseguir todos los paquetes turísticos en la DB
