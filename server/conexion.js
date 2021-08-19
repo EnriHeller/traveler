122bc717d505eaa89111c8e6590ef476291fd6dc
Max <mdelarosatoroag@gmail.com>
2021-08-19 13:18:04 -0500

    cambiado el connectionString para conectarse a la BD en la nube de mongoDB

--- server/conexion.js
+++ server/conexion.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const host = "localhost";
 const port = 27017;
 const dbName = "traveller";
-const connectionString = `mongodb://${host}:${port}/${dbName}`;
+const username = "admin";
+const password = "admin";
+// const connectionString = `mongodb://${host}:${port}/${dbName}`;
+
+const connectionString = `mongodb://${username}:${password}@traveler-shard-00-00.ikm1y.mongodb.net:27017,traveler-shard-00-01.ikm1y.mongodb.net:27017,traveler-shard-00-02.ikm1y.mongodb.net:27017/test?replicaSet=atlas-1xat86-shard-0&ssl=true&authSource=admin`
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
