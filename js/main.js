b5f28660638135f72df18ee7527869bad34a0d4d
Max <mdelarosatoroag@gmail.com>
2021-08-19 20:00:46 -0500

    arreglado relative link al registrar usuario

--- js/main.js
+++ js/main.js
@@ -338,7 +338,7 @@ var requestOptions = {
 
 fetch("http://localhost:3000/signUp", requestOptions)
   .then(response => response.json())
-  .then(result => window.location.replace("/login.html"))
+  .then(result => window.location.replace("./login.html"))
   .catch(error => console.log('error', error));
 })
 
